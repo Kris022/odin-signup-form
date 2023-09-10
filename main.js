8c833e10cd7ccf9105f34f307067e94518ffd6bd
Kris <kharonek@yahoo.com>
2023-09-09 22:27:53 -0300

    fialising form styles

---
+++ main.js
@@ -0,0 +1,6 @@
+// const password = document.querySelector("#password").value;
+// const confirmPassword = document.querySelector("#password-confirm").value;
+
+// if (password !== confirmPassword) {
+
+// }
